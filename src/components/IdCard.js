45a883e6f56676ef390aff28adc057ae9d3f0b28
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-09 17:20:17 -0300

    updated

---
+++ src/components/IdCard.js
@@ -0,0 +1,21 @@
+
+
+function IdCard(props) {
+    return (
+        <div className="container">
+            <div className="imagem">
+               <img src={props.picture} alt="foto do cliente" /> 
+            </div>
+            <div className="texto">
+                <p>First Name: <span>{props.firstName}</span></p>
+                <p>Last Name: <span>{props.lastName}</span></p>
+                <p>Gender: <span>{props.gender}</span></p>
+                <p>Height: <span>{props.height}</span></p>
+                <p>Birth: <span>{props.birth}</span></p>
+            </div>
+
+        </div>
+    )
+}
+
+export default IdCard;
