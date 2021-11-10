298719131eb4065f3a6ac8c40f9e07d7898004be
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-10 13:57:45 -0300

    created function credit card

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,37 +1,35 @@
 import mastercard from './img/master-card.svg';
 import visa from './img/visa.png'
 
-/*
-type: A string that can be "Visa" or "Master Card"
-number: A string that is a number of the credit card. You will only display the 4 last digits for security reasons wink
-expirationMonth: A number that represents the month, between 1 and 12
-expirationYear: A number that represents the year
-bank: A string that represents the name of the bank
-owner: A string that represents the name of the owner
-bgColor: A string for the background color of the card
-color: A string for the text color of the card
-*/
-
 function CreditCard(props) {
+    function creditCardType(brand) {
+        if (brand === 'Visa') {
+            return `{<img src={visa}/>}`
+        } else {
+            return `{<img src={mastercard}/>}`
+        }
+    }
     return(
         <div>
-            {/* <img src={mastercard}/> */}
-
+            <span>{creditCardType(props.type)}</span>
             <span>**** **** **** {props.number.split('').slice(12).join('')}</span>
             <span>Expires {props.expirationMonth.toString().padStart(2, '0')}/{props.expirationYear.toString().split("").splice(2,2).join("")}</span>
+            <span>{props.bank}</span>
+            <span>{props.owner}</span>
+            <span style={{ backgroundColor: props.bgColor, color: props.color, height: '100px', width: '400px'}}></span>
         </div>
+       
     )
 }
 
 export default CreditCard;
-
 /*
 type="Visa"
-          number="0123456789018845"
-          expirationMonth={3}
-          expirationYear={2021}
-          bank="BNP"
-          owner="Maxence Bouret"
-          bgColor="#11aa99"
-          color="white" 
-          */
+number="0123456789018845"
+expirationMonth={3}
+expirationYear={2021}
+bank="BNP"
+owner="Maxence Bouret"
+bgColor="#11aa99"
+color="white" 
+*/
