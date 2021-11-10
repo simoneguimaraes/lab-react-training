e8bd02ec9c5f51f79416863aa2c2fa972d85a95e
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-10 16:33:25 -0300

    updated driver card

--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,5 +1,27 @@
-function DriverCard()
-
-
+function DriverCard(props) {
+    function createStars(rate) {
+        if (rate === 0) {
+            return `☆☆☆☆☆`
+        } else if (rate === 1) {
+            return `★☆☆☆☆`
+        } else if (rate === 2) {
+            return `★★☆☆☆`
+        } else if (rate === 3) {
+            return `★★★☆☆`
+        } else if (rate === 4) {
+            return `★★★★☆`
+        } else {
+            return `★★★★★`
+        }
+    }
+    return(
+        <div>
+            <img src={props.img} style={{width: '100px'}} />
+            <span>{props.name}</span>
+            <span>{createStars(Math.round(props.rating))}</span>
+            <span>{props.car.model} - {props.car.licensePlate}</span>       
+        </div>
+    )
+}
 
 export default DriverCard;
