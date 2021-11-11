a947e89919d3c1df4691682766d1af1d06941a2f
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-11-11 10:49:44 -0300

    updated carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,24 +2,25 @@ import { useState } from "react";
 
 function Carousel(props) {
     const [images, setRandomImage] = useState(0)
-    
+    setRandomImage(props.imgs[Math.floor((Math.random() * 3))]) 
+
     function rightClick() {
-        if(images > -1 && images < 5) {
-            setRandomImage(images + 1)
+        if(images < props.imgs[images] - 1) {
+        setRandomImage(images + 1)
         }
     }
-
+    
     function leftClick() {
-        if(images > -1 && images < 5) {
-            setRandomImage(images - 1)
+        if(images < props.imgs[images] - 1) {
+        setRandomImage(images - 1)
         }
     }
 
     return(
         <div>
-            <button style={{fontSize: '25px', backgroundColor: 'green'}} onClick={rightClick}>Left</button>
+            <button style={{fontSize: '25px', backgroundColor: 'green'}} onClick={leftClick}>Left</button>
             <img style={{height: '255px', width: '255px'}} src={props.imgs[images]} alt='picture'/>
-            <button style={{fontSize: '25px', backgroundColor: 'green'}} onClick={leftClick}>Right</button>
+            <button style={{fontSize: '25px', backgroundColor: 'green'}} onClick={rightClick}>Right</button>
         </div>
     )
 }
